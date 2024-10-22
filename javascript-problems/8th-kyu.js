b055478af19e2dfb18974457e3084131716112c2
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-10-22 14:12:06 -0700

    Testing;

--- javascript-problems/8th-kyu.js
+++ javascript-problems/8th-kyu.js
@@ -53,6 +53,12 @@ We're testing basic loops and math operations. This is for beginners who are jus
 Advanced users may find this extremely easy and can easily write this in one line.
 */
 function sumArrays(numbers) {
+  // let sum = 0;
+  // for (let el of numbers) {
+  //     sum += el;
+  // }
+  // return sum;
+
   return numbers.reduce((a, b) => a + b, 0);
 }
 console.log("sumArrays: " + sumArrays([1, 5.2, 4, 0, -1]));
