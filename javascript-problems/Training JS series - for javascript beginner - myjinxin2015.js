9368446d543b06ccef694d29e5cb9077c6d8beca
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-10-23 12:59:15 -0700

    Added Training JS series

---
+++ javascript-problems/Training JS series - for javascript beginner - myjinxin2015.js
@@ -0,0 +1,53 @@
+/* -------------------- Training JS #1: create your first JS function and print "Hello World!" --------------------
+In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+Look at this example:
+
+      --------keyword "function"
+      |       ----your function name
+      |       |    ---if needed, some arguments will appear in parentheses
+      |       |    |
+    function myfunc(){  ---------your function code will starting with "{"
+      //you should type your code here
+    }----------------------------ending with "}"
+If we want to print some to the screen, maybe we can use Document.write() in the web, or use alert() pop your message, but Codewars did not support these methods, we should use console.log() in your function. see this example:
+
+function printWordToScreen(){
+  var somewords="This is an example."
+  console.log(somewords)
+}
+If we run this function, This is an example. will be seen on the screen. As you see, console.log() is an useful method. You will use it a lot!
+
+Task
+Please refer to two example above and write your first JS function.
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line (don't forget to put the str in the parentheses).
+*/
+helloWorld = function () {
+  var str = "Hello World!";
+  console.log(str);
+};
+helloWorld();
+
+/* -------------------- Training JS #2: Basic data types--Number --------------------
+In javascript, Number is one of basic data types. It can be positive: 1,2,3, negative:-1,-100 , integer:123,456, decimal:3.1415926,-8.88 etc..
+
+Numbers can use operators such as + - * / %
+
+Task
+I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
+*/
+
+/* -------------------- Training JS #3: Basic data types--String --------------------
+
+*/
