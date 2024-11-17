d836b8e916cf63e1cc1c6e3b51d3f46546e0e0db
jwong209 <68797448+jwong209@users.noreply.github.com>
2024-11-17 12:49:55 -0800

    Added to 6th kyu solution for count()

--- javascript-problems/6th-kyu_01.js
+++ javascript-problems/6th-kyu_01.js
@@ -62,3 +62,49 @@ function duplicateCount(text){
 }
 // console.log(duplicateCount("abcde")); // >>> 0
 // console.log(duplicateCount("ABBA")); // >>> 2
+
+/* ---------------------------------------- Count characters in your string ----------------------------------------
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+*/
+function count(string) {
+  if (string.length === 0) {
+      return {};
+  }
+
+  let obj = new Object();
+
+  for (let i = 0; i < string.length; i++) {
+     if (obj[string[i]] === undefined) {
+          obj[string[i]] = 1;
+     } else {
+          obj[string[i]] += 1;
+     }
+  }
+  return obj;
+}
+
+/* ---------------------------------------- nnn ----------------------------------------
+
+*/
+
+/* ---------------------------------------- nnn ----------------------------------------
+
+*/
+
+/* ---------------------------------------- nnn ----------------------------------------
+
+*/
+
+/* ---------------------------------------- nnn ----------------------------------------
+
+*/
+
+/* ---------------------------------------- nnn ----------------------------------------
+
+*/
+
+/* ---------------------------------------- nnn ----------------------------------------
+
+*/
